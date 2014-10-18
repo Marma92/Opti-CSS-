11db7fc059b5f7fc01d126f1e2c5b67ff261fe39
Arnaud <Arnaud@L33TM4CH1NE>
2014-10-18 22:54:12 +0200

    test

---
+++ CppApplication_1/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: Arnaud
+ *
+ * Created on 18 octobre 2014, 22:53
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
