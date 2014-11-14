654c3f0781f8335539faae2797c8f0151fc84349
ileossa <swip_delta@hotmail.fr>
2014-11-14 17:56:24 +0100

    remove manipulation_liste.h
    inutile, a l avenir mettre tout dans fonction.h

--- Opti-CSS/fonctions.h
+++ Opti-CSS/fonctions.h
@@ -18,3 +18,12 @@ void file_exist( char name_file );
 int is_css_file (char * file_input);
 void write( char out_file, t_list_chain* liste );
 FILE* file_open(char* file_open);
+
+
+/*--------------------------------------------------\*
+ declaration des prototypes de manipulation_liste.c
+\*--------------------------------------------------*/
+t_list_chain* list_chain_new(char balise, char value);
+t_list_chain* list_chain_append(t_list_chain* list, char balise, char value);
+t_list_chain* list_chain_remove(t_list_chain* list, int index);
+t_list_chain* list_chain_display(t_list_chain* list);
--- Opti-CSS/manipulation_liste.h
+++
@@ -1,7 +0,0 @@
-/*--------------------------------------------------\*
- declaration des prototypes de manipulation_liste.c
-\*--------------------------------------------------*/
-t_list_chain* list_chain_new(char balise, char value);
-t_list_chain* list_chain_append(t_list_chain* list, char balise, char value);
-t_list_chain* list_chain_remove(t_list_chain* list, int index);
-t_list_chain* list_chain_display(t_list_chain* list);
