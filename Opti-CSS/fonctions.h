2ac35bb0c0abd1d70c8f7b3e2fcf6bbca0491ecd
arnaudf75 <mail.arnaudflaesch@gmail.com>
2014-11-17 12:26:32 +0100

    Update fonctions.h

--- Opti-CSS/fonctions.h
+++ Opti-CSS/fonctions.h
@@ -1,10 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-
-/*--------------------------------------------------\*
- declaration des prototypes de fonction.c
-\*--------------------------------------------------*/
+//declaration des prototypes de fonction.c
 void file_exist(const char * name_file);
-int is_css_file (const char * file_input);
-//void write( char out_file, t_list_chain* liste );			// a reecrire
-void file_open(FILE* fichier, char* file_open);							// appeler la fonction tel-que =>   FILE* fichier = NULL;   file_open(fichier, "toto");
-void print_help();
+void is_css_file (const char * file_input);
+//void write( char out_file, t_list_chain* liste );
+//FILE* file_open(char* file_open);
