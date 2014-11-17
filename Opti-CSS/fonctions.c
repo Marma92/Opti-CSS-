956d1140ba39477a05cc7b0c3f3f3c7fee1c5034
arnaudf75 <mail.arnaudflaesch@gmail.com>
2014-11-17 12:26:14 +0100

    Update fonctions.c

--- Opti-CSS/fonctions.c
+++ Opti-CSS/fonctions.c
@@ -1,39 +1,38 @@
-
+#include "fonctions.h"
 
 //permet de verifier si le fichier cible exist.
+
 void file_exist(const char * name_file) {
     FILE* fp;
     fp = fopen(name_file,"r");
     if(NULL == fp ){
         printf("File %s does not exists.\n",name_file);
-        fclose( fp );
+        fclose(fp);
         exit(1);
     }
 }
 
 //permet de verifier si le fichier en entree est bien de type .css
-int is_css_file (const char * file_input) {
+
+void is_css_file (const char * file_input) {
     int i = 0;
     while (file_input[i] != '\0') {
         i++;
     }
-    if (file_input[i-1]=='s' && file_input[i-2]=='s' && file_input[i-3]=='c' && file_input[i-4]=='.' ) {
-        return(1);
-    }
-    else {
-        printf("File %s isn't a css file.",file_input);
-        return(0);
+    if (file_input[i-1]!='s' || file_input[i-2]!='s' || file_input[i-3]!='c' || file_input[i-4]!='.' ) {
+        printf("File %s isn't a css file.\n",file_input);
+        exit(1);
     }
 }
 
+/*
+	ouverture du fichier cible
 
-
-//ouverture du fichier cible
-void file_open(FILE* fichier, char* file_open){
+FILE* file_open(char* file_open){
 	file_exist(out_file);
 	is_css_file(file_open);
 	//obliger de passer l'option w+, pb de droit sous linux
-	//FILE* fichier = NULL;
+	FILE* fichier = NULL;
 
     fichier = fopen( out_file, "r+");
 
@@ -47,7 +46,16 @@ void file_open(FILE* fichier, char* file_open){
 }
 
 
-//Fonction permettant, l'ecriture dans un fichier et il le creer s'il nexiste pas.
+char* get_line(FILE* file_open){
+	file_open(file_open);
+}
+
+
+
+
+
+	Fonction permettant, l'ecriture dans un fichier	et il le creer s'il nexiste pas.
+
 void write( char out_file, t_list* liste ){
 	//FILE* fw;
 	file_exist(out_file);
@@ -64,9 +72,11 @@ void write( char out_file, t_list* liste ){
 
 
 void print_help(){
-	printf("Voici la liste des options disponnibles pour l application\n");
-	printf("\t-h\tpermet d'afficher le message du helper\n");
+	printf("Voici la liste des options disponnible pour l application\n");
+	printf("\t-h\tpermet dáfficher le message du helper\n");
 	printf("\t-b\tpermet de mettre en forme le texte de sortie\n");
 }
 
 
+void get_line
+ */
