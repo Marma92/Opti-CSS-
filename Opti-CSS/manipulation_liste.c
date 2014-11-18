46ab69dab32654fb6caa5c38c78ee23f7d6b16a4
arnaudf75 <mail.arnaudflaesch@gmail.com>
2014-11-18 16:50:08 +0100

    Update manipulation_liste.c

--- Opti-CSS/manipulation_liste.c
+++ Opti-CSS/manipulation_liste.c
@@ -9,6 +9,7 @@ t_list_chain * list_chain_new(const char * balise){
     list->balise = balise;
     list->node = NULL;
     list->next = NULL;
+    list_chain_display(list);
     return list;
 }
 
@@ -21,23 +22,53 @@ t_list_node* list_node_new(const char* property, const char* value){
 }
 
 
+int equals (const char * chaine1,const char * chaine2) {
+    
+    int i=0;
+    while (chaine1[i] != '\0' || chaine2[i]!='\0') {
+        printf("%c = %c\n", chaine1[i], chaine2[i]);
+        if (chaine1[i] != chaine2[i]) {
+            return (0);
+        }
+        i++;
+    }
+    return (1);
+}
+
+
 // rajoute un element a la fin de la liste
 t_list_chain * list_chain_append(t_list_chain*  list, const char* balise, const char* property, const char* value){
     
-    if(!list)
+    if(!list) {
+        printf("0-\n");
         list = list_chain_new(balise);
+    }
     //test si balise existe deja
     do{
-        if(list->balise == balise){
-           do{
-                if(list->node->property == property && list->node->value == value){
-                    continue;
-                }else{
+        printf("1-\n");
+        if( equals( list->balise, balise )){
+            printf("2-\n");
+            if (!list->node) {                
+                list->node = list_node_new(property, value);                
+            }
+            //printf("pointeur ");
+            //int toto = equals(list->node->property, property);
+            //printf("2bis- %d, %d\n", equals(list->node->property, property), equals(list->node->value,value));
+            do{
+                
+                if(!equals(list->node->property, property) && !equals(list->node->value,value)){
+                    printf("4-\n");
                     list_node_new(property, value);
+                }
+                else{
+                    printf("3-\n");
+                    break;
+                    
                 }
                 list->node = list->node->next;
             }while(list->node != NULL);
-        }else{
+        }
+        else{
             list_chain_new(balise);
         }
         list = list->next;
@@ -68,11 +99,14 @@ t_list_chain* list_chain_remove(t_list_chain* list, int index){
 
 //affiche la liste en cas de debog
 void list_chain_display(t_list_chain* list){
+    printf ("1- Dislay\n");
     if(list){
+        printf ("2- Dislay\n");
         while(list != NULL){
+            printf ("3- Dislay\n");
             printf("Balise %s\n",list->balise);
             while (list->node!=NULL){
-                printf("Propriété %s Valeur %s\n");
+                printf("Propriété %s Valeur %s\n",list->node->property,list->node->value);
                 list->node = list->node->next;
             }
             list = list->next;
@@ -82,7 +116,7 @@ void list_chain_display(t_list_chain* list){
 
 
 // Copie un mot,une propriété ou une valeur
-char * word_copy (char * word) {
+char * word_copy (const char * word) {
     int i=0;
     char * word_copy;
     word_copy = (char *)malloc(sizeof(word));
@@ -99,3 +133,4 @@ char * word_copy (char * word) {
     
     return (word_copy);
 }
+
