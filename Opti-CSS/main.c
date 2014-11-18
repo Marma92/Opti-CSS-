a07c6a09a4af6259578eb8c0751c615b2bf8f56b
arnaudf75 <mail.arnaudflaesch@gmail.com>
2014-11-18 13:44:54 +0100

    Update main.c

--- Opti-CSS/main.c
+++ Opti-CSS/main.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct s_list_chain {
-    char* balise;
-    char* property;
-    char* value;
-    struct s_list_chain* next;
-}t_list_chain;
-
-#include "fonctions.h"
 #include "manipulation_liste.h"
+#include "fonctions.h"
+
+
 #define OPTION_BEAUTIFUL 0
 #define TRUE 1
 #define FALSE 0
 
-
 int main(int argc, char** argv) {
-
-    int option_beautiful = OPTION_BEAUTIFUL;
-    int i;
+    
+    int option_beautiful = FALSE;
+    int i = 0;
+    int i_files=1; // Variable incrémentée à chaque fois qu'on ouvre un nouveau fichier
+    char lecture,*balise,*property,*value,word[256]; // Caractère lu par la fonction fgetc, cela permet d'afficher le contenu du fichier
+    const char *filename,* css_file_input,* css_file_output; // Respectivement les variables qui vont contenir les noms des fichiers 
+                                                   // d'entrée et de sortie
+    FILE *file_input,*file_output;
     t_list_chain * list = NULL;
-
+    argc = 4; // Comme le fait d'aller chercher la console à chaque fois
+              // pour éxécuter le programme est long,je passe les noms des fichiers en paramètres de cette façon
+    argv[1,1] = "style.css"; // Fichier d'entrée
+    argv[1,2] = "style2.css"; // Fichier d'entrée
+    argv[1,3] = "result.css"; // Fichier de sortie
 
     if (2 >= argc) {
         if (45== argv[1][0] ){
@@ -39,94 +42,52 @@ int main(int argc, char** argv) {
 
     if (3 > argc) { // On vérifie qu'on a bien au moins un fichier entrée et un fichier sortie en paramètres
         printf("Not enough parameters.\n");
-        return (-1);
+        return (1);
     }
-
-    //file in
-    while(i=0 >= argc-2){
-        
-        //si fichier css, on le stocke dans le tableau file
-        if ( is_css_file(argv[i]) == TRUE && file_exist(argv[i])){
-            FILE* file_input = fopen(argv[i], "r");
-            if( NULL == file_input){
-                printf("Cannot read from file: %s.\n", argv[i]);
-                fclose(argv[i]);
-                exit(-1);
-            }
-            else{
-                while ((lecture=fgetc(file_input))!= EOF) {
-                    switch (lecture) {
-                        case '{' : 
-                            word[i_word]='\0';
-                            balise = word_copy(word);
-                            word[0]='\0';
-                            i_word=0;
-                            break;
-                        case ':' : 
-                            word[i_word]='\0';
-                            property = word_copy(word);
-                            word[0]='\0';
-                            i_word=0;
-                            break;
-                        case ';' : 
-                            word[i_word]='\0';
-                            value = word_copy(word);
-                            word[0]='\0';
-                            i_word=0;
-                            list = list_chain_append(list,balise,property,value);
-                            break;
-                        case '}' : 
-                            word[i_word]='\0';
-                            word[0]='\0';
-                            i_word=0;
-                            break;
-                        case 32 : 
-                            break;
-                        case 9 : 
-                            break;
-                        case '\n' : 
-                            break;
-                        default : 
-                            word[i_word]=lecture;
-                            i_word++; 
-                            break;
-                    }
+  // On vérifie l'extension des fichiers et leur existence
+    while (i_files<argc-1) {
+        filename = argv[1,i_files];
+        file_exist(filename);
+        is_css_file(filename);                
+        i_files++;
+    }
+    // On vérifie si le fichier de sortie existe,si c'est le cas on arrête
+    filename = argv[1,i_files];
+    if (NULL != (file_output = fopen(filename,"r"))) {
+        printf("Output file %s already exists.\n",filename);
+        fclose(file_output);
+    }
+    else {
+        css_file_output = filename;
+        fclose(file_output);
+    }
+    for (i_files=1;i_files<argc-1;i_files++) { // On parcourt les fichiers
+        css_file_input = argv[1,i_files];
+        file_input = fopen(css_file_input,"r");
+        if (NULL == file_input) {
+            printf("Cannot read from file %s.\n",css_file_input); // Si l'un des fichiers d'entrée n'existe pas,on arrête
+            fclose(file_input);
+            return (1);
+        }
+        else {
+            int i_word = 0;
+            while ((lecture=fgetc(file_input))!= EOF) { // On parse
+                switch (lecture) {
+                    case '{' : word[i_word]='\0';balise = word_copy(word);word[0]='\0';i_word=0;break;
+                    case ':' : word[i_word]='\0';property = word_copy(word);word[0]='\0';i_word=0;break;
+                    case ';' : word[i_word]='\0';value = word_copy(word);word[0]='\0';i_word=0;
+                                list = list_chain_append(list,balise,property,value);
+                                break;
+                    case '}' : word[i_word]='\0';word[0]='\0';i_word=0;break;
+                    case 32 : break;
+                    case 9 : break;
+                    case '\n' : break;
+                    default : word[i_word]=lecture;i_word++; break;
                 }
             }
             fclose(file_input);
         }
-        else{
-            printf("Error this file: %c is not a CSS file", argv[i]);
-            exit(-1);
-        }
-        i++;
     }
-
-    //file result out
-    FILE* file_result = fopen(argv[argc-1], "w+");
-    
-    if(is_css_file(file_result)){
-        if (file_result(file_result) == NULL){
-            printf("Error open file result.\n");
-            exit(-1);
-        }
-        /* write(file_result, chaine);
-        list->balise
-        list->property
-        list->value
-        */
-        
-        while(list->next == NULL){
-            write(file_result, list->balise);
-            if (TRUE == option_beautiful)
-                write(file_result, "{\n\t");
-            write(file_result, list->property);
-            if (TRUE == option_beautiful)
-                write(file_result, "\n");
-            write(file_result, list->property);
-            list = list->next;
-        }
-       
-    }
+    list = list_sort(list);
     return (EXIT_SUCCESS);
 }
