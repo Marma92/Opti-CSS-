a8487dc15aafcce2e8d9e7c57ab746b9b30972a7
arnaudf75 <mail.arnaudflaesch@gmail.com>
2014-11-18 14:39:39 +0100

    Update manipulation_liste.h

--- Opti-CSS/manipulation_liste.h
+++ Opti-CSS/manipulation_liste.h
@@ -3,7 +3,7 @@
 
 typedef struct s_list_chain {
     char* balise;
-    struct t_list_node * node;
+    struct s_list_node * node;
     struct s_list_chain * next;
 }t_list_chain;
 
@@ -19,10 +19,10 @@ typedef struct s_list_node {
  declaration des prototypes de manipulation_liste.c
 \*--------------------------------------------------*/
 
-/* A retravailler */t_list_chain * list_chain_new(char * balise,char * property,char * value);
-/* A retravailler */ t_list_chain * list_chain_append(t_list_chain* list,char * balise,char * property,char * value);
-/*A retravailler */ t_list_chain * list_chain_remove(t_list_chain* list, int index);
-/*A retravailler */t_list_chain * list_sort (t_list_chain * list);
+t_list_chain * list_chain_new(char * balise);
+t_list_chain * list_chain_append(t_list_chain* list,char * balise,char * property,char * value);
+t_list_chain * list_chain_remove(t_list_chain* list, int index);
+t_list_chain * list_sort (t_list_chain * list);
 void list_chain_display(t_list_chain* list);
 char * concat(char *chaine1,char *chaine2);
 char * word_copy (char * word);
