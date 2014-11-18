dac2e9228adce2c79462512f7cbb3df9cf6d861f
arnaudf75 <mail.arnaudflaesch@gmail.com>
2014-11-18 13:44:29 +0100

    Update manipulation_liste.h

--- Opti-CSS/manipulation_liste.h
+++ Opti-CSS/manipulation_liste.h
@@ -3,10 +3,15 @@
 
 typedef struct s_list_chain {
     char* balise;
-    char* property;
-    char* value;
-    struct s_list_chain* next;
+    struct t_list_node * node;
+    struct s_list_chain * next;
 }t_list_chain;
+
+typedef struct s_list_node {
+    char * property;
+    char * value;
+    struct s_list_node * next;
+};
 
 
 
@@ -14,9 +19,10 @@ typedef struct s_list_chain {
  declaration des prototypes de manipulation_liste.c
 \*--------------------------------------------------*/
 
-t_list_chain * list_chain_new(char * balise,char * property,char * value);
-t_list_chain * list_chain_append(t_list_chain* list,char * balise,char * property,char * value);
-t_list_chain * list_chain_remove(t_list_chain* list, int index);
+/* A retravailler */t_list_chain * list_chain_new(char * balise,char * property,char * value);
+/* A retravailler */ t_list_chain * list_chain_append(t_list_chain* list,char * balise,char * property,char * value);
+/*A retravailler */ t_list_chain * list_chain_remove(t_list_chain* list, int index);
+/*A retravailler */t_list_chain * list_sort (t_list_chain * list);
 void list_chain_display(t_list_chain* list);
-
+char * concat(char *chaine1,char *chaine2);
 char * word_copy (char * word);
