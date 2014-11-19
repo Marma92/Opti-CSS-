caa5cbbcdff2b7e0765415ab0212cdaae9a62264
Marma92 <elbaz.amram@gmail.com>
2014-11-19 06:40:32 +0100

    prototype de write, à finir
    HELP!

---
+++ Opti-CSS/prototype du write2.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct fichier_final {
+    struct elements* e;
+};
+
+struct elements {
+	char* nomElement
+    struct properties* p;
+    struct elements* next;
+};
+
+struct properties {
+    char* propname;
+    char* value;
+    struct properties* next;
+};
+
+fichier_final* int_fichier_final(element* e){
+    fichier_final* ff = (t_list_chain *)malloc(sizeof(fichier_final));    
+    ff->e = e;
+    return ff;
+}
+
+elements* element_new(char* nom, properties* p){
+    element_new* elem = (t_list_chain *)malloc(sizeof(element_new));    
+    elem->nomElement = nom
+    elem->p = p;
+    elem->next = NULL;
+    return elem;
+}
+
+properties* prop_new(char* propName, char* value){
+    properties* prop = (t_list_chain *)malloc(sizeof(properties));    
+    prop->propName = propName;
+    prop->value = value;
+    prop->next = NULL;
+    return prop;
+}
+
+elements* elements_append(elements* list, properties* p){
+    if(!list) {
+        return element_new(char* nom, properties* p);
+    }
+    elements* save = list;
+    while(save->next != NULL){
+        save = save->next;
+    }
+    save->next = element_new(char* nom, properties* p);
+    return list;
+}
+
+properties* properties_append(properties* list, char* propName, char* value){
+    if(!list) {
+        return prop_new(char* propName, char* value);
+    }
+    properties* save = list;
+    while(save->next != NULL){
+        save = save->next;
+    }
+    save->next = prop_new(char* propName, char* value);
+    return list;
+}
+
+void write_to_file(fichier_final* ff){
+	elements* e = ff->e
+	while(e->next != NULL){
+		file_append(f, e->nom)
+		file_append(f, "{")
+		properties* p = e->p
+		while(p->next != NULL){
+			file_append(f, p->propName)
+			file_append(f, " : ")
+			file_append(f, p->value)
+			file_append(f, ";")
+		}
+		file_append(f, e->nom)
+		file_append(f, "}")
+	}
+}
+
+void file_append(char* fileName, char* toWrite){
+	FILE *f;
+	f = fopen(fileName, "a");
+	while(!feof(f)) { //1
+        while(fgets(toWrite, sizeof(toWrite), f)) { //2
+        	fprintf(f, "%s", toWrite);
+    	}
+    }
+}
+
+
+
+
